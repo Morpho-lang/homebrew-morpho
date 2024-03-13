9d1159a1fda8d32b52c9d7eab770a8c6be92977f
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2024-03-13 15:09:37 -0500

    Create morpho-cli.rb

---
+++ Formula/morpho-cli.rb
@@ -0,0 +1,26 @@
+class Morpho_CLI < Formula
+  desc "Morpho-cli. A terminal application for the morpho language"
+  homepage "https://github.com/morpho-lang/morpho-cli"
+  url "https://github.com/Morpho-lang/morpho-cli/archive/refs/tags/v0.6.0-alpha.tar.gz"
+  sha256 "1a659aff0e23f82aa69aa0fcda1aee4ca58dd72372ab901dfc8cde5ad8c20f40"
+  license "MIT"
+
+  depends_on "libgrapheme"
+
+  def install
+    Dir.mkdir("build")
+    chdir "build" do 
+      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DMORPHORESOURCESDIR=#{prefix}", ".."
+      system "make"
+
+      bin.install "morpho6"
+    end
+  end
+
+  test do
+    (testpath/"hello.morpho").write <<~EOS
+      print "Hello, world!"
+    EOS
+    assert_equal "Hello, world!", shell_output("#{bin}/morpho6 hello.morpho").strip.gsub(/\e\[(\d+)(;\d+)*m/, '') # Remove terminal codes
+  end
+end
