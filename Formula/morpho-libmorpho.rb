841c8378d3c2ce26e672d1cd1ade2c27c1c9db49
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2024-03-17 19:55:24 -0700

    Correct sha256

--- Formula/morpho-libmorpho.rb
+++ Formula/morpho-libmorpho.rb
@@ -2,7 +2,7 @@ class MorphoLibmorpho < Formula
   desc "Morpho-libmorpho. Morpho built as a shared library."
   homepage "https://github.com/Morpho-lang/morpho-libmorpho"
   url "https://github.com/Morpho-lang/morpho-libmorpho/archive/refs/tags/v0.6.0-alpha.tar.gz"
-  sha256 "1cef026fca37289ab32fbda634c0708199b6b6714f7878ba2059b68e5f0f"
+  sha256 "2a281cef026fca37289ab32fbda634c0708199b6b6714f7878ba2059b68e5f0f"
   license "MIT"
 
   depends_on "cmake" => :build
