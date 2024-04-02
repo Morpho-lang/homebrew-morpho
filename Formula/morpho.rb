23189585d8feba7c23c00844ceebfb67cece75ee
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2024-04-02 09:00:21 -0400

    Update morpho metapackage

--- Formula/morpho.rb
+++ Formula/morpho.rb
@@ -5,49 +5,17 @@ class Morpho < Formula
   sha256 "a3e4c0e4fb119a6a081a25178d00cb03959767c28ce1e2ffcff280f5e89979d2"
   license "MIT"
 
-  depends_on "glfw" 
-  depends_on "suite-sparse"
-  depends_on "freetype"
-  depends_on "povray"
+  depends_on "morpho-libmorpho" 
+  depends_on "morpho-cli"
+  depends_on "morpho-morphoview"
 
   def install
-    chdir "morpho5" do 
-      if OS.mac?
-        if Hardware::CPU.arm?
-          system "make", "MORPHORESOURCESDIR=#{prefix}", "-f", "Makefile.m1"
-        else 
-          system "make", "MORPHORESOURCESDIR=#{prefix}", "-f", "Makefile"
-        end 
-      else
-        system "make", "MORPHORESOURCESDIR=#{prefix}", "-f", "Makefile.linux"
-      end
-
-      bin.install "morpho5"
-      (share/"modules").install Dir["modules/*"]
-      (share/"help").install Dir["docs/*"]
-      (include/"morpho").install Dir["**/*.h"]
-    end
-
-    chdir "morphoview" do
-      ENV["CPATH"] = "#{include}/morpho" # Make sure header files are visible in include path 
-      if OS.mac?
-        if Hardware::CPU.arm?
-          system "make", "-f", "Makefile.m1"
-        else 
-          system "make", "-f", "Makefile"
-        end 
-      else
-        system "make", "-f", "Makefile.linux"
-      end
-
-      bin.install "morphoview"
-    end
   end
 
   test do
     (testpath/"hello.morpho").write <<~EOS
       print "Hello, world!"
     EOS
-    assert_equal "Hello, world!", shell_output("#{bin}/morpho5 hello.morpho").strip.gsub(/\e\[(\d+)(;\d+)*m/, '') # Remove terminal codes
+    assert_equal "Hello, world!", shell_output("#{bin}/morpho6 hello.morpho").strip.gsub(/\e\[(\d+)(;\d+)*m/, '') # Remove terminal codes
   end
 end
