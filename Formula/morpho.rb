af55c51f5ca72f50b5552bfce2a8fca87d58e863
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2024-04-02 10:15:20 -0400

    Update morpho.rb

--- Formula/morpho.rb
+++ Formula/morpho.rb
@@ -1,10 +1,19 @@
 class Morpho < Formula
   desc "The Morpho language. Morpho is a small embeddable language for scientific computing applications."
   homepage "https://github.com/Morpho-lang/morpho"
+  url "https://github.com/Morpho-lang/morpho/archive/refs/tags/v0.6.0-alpha.tar.gz"
+  sha256 "7aedbb7dae8ebd41634bab7ed25716cd6dedc2668c3ffc72c3d86506178dc0bb"
+  license "MIT"
 
   depends_on "morpho-libmorpho" 
   depends_on "morpho-cli"
   depends_on "morpho-morphoview"
+
+  def install
+    (testpath/"hello.morpho").write <<~EOS
+      print "Hello, world!"
+    EOS
+  end
 
   test do
     (testpath/"hello.morpho").write <<~EOS
