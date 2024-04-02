f1978b9737723abd95101f3f93df42e26af0af2c
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2024-04-02 10:11:44 -0400

    Remove file

--- Formula/morpho.rb
+++ Formula/morpho.rb
@@ -1,16 +1,10 @@
 class Morpho < Formula
   desc "The Morpho language. Morpho is a small embeddable language for scientific computing applications."
   homepage "https://github.com/Morpho-lang/morpho"
-  url "https://github.com/Morpho-lang/morpho/archive/refs/tags/v0.5.7.tar.gz"
-  sha256 "a3e4c0e4fb119a6a081a25178d00cb03959767c28ce1e2ffcff280f5e89979d2"
-  license "MIT"
 
   depends_on "morpho-libmorpho" 
   depends_on "morpho-cli"
   depends_on "morpho-morphoview"
-
-  def install
-  end
 
   test do
     (testpath/"hello.morpho").write <<~EOS
