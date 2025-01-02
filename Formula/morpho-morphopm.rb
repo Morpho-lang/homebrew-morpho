197e8336ee29a45fe50fd048349d11cd022d12fe
softmattertheory <timothy.atherton@tufts.edu>
2025-01-01 20:07:02 -0500

    Add morphopm formula

---
+++ Formula/morpho-morphopm.rb
@@ -0,0 +1,19 @@
+class Morphopm < Formula
+  desc "Morpho-morphom. Simple package manager for the morpho language"
+  homepage "https://github.com/morpho-lang/morpho-morphopm"
+  url "https://github.com/Morpho-lang/morpho-morphopm/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  license "MIT"
+
+  depends_on "cmake" 
+  depends_on "morpho"
+  depends_on "morpho"
+
+  def install
+    bin.install "morphopm"
+  end
+
+  test do
+    assert_equal "0.1.0", shell_output("#{bin}/morphopm version").strip.gsub(/\e\[(\d+)(;\d+)*m/, '') # Remove terminal codes
+  end
+end
