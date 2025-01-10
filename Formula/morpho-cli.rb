2e725c9dcbb3f55268e1a318c179a3e21102c65d
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2025-01-10 14:52:15 -0800

    Update morpho-cli.rb

--- Formula/morpho-cli.rb
+++ Formula/morpho-cli.rb
@@ -2,7 +2,7 @@ class MorphoCli < Formula
   desc "Morpho-cli. A terminal application for the morpho language"
   homepage "https://github.com/morpho-lang/morpho-cli"
   url "https://github.com/Morpho-lang/morpho-cli/archive/refs/tags/v0.6.2-alpha2.tar.gz"
-  sha256 "f48166496c118bb7bd2296ae1e8b4a510acffc477097072058910453f191baaf"
+  sha256 "6014736f64271f1a2fb53eada8885cc70ef71f2234a3b6c221e756587116e4e9"
   license "MIT"
 
   depends_on "libgrapheme" => :recommended
