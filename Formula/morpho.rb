57cb8d6b4e8a1a456438c2a4334fe48eb07f67df
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2025-01-10 14:41:08 -0800

    Update morpho.rb

--- Formula/morpho.rb
+++ Formula/morpho.rb
@@ -1,7 +1,7 @@
 class Morpho < Formula
   desc "The morpho language built as a shared library."
   homepage "https://github.com/Morpho-lang/morpho"
-  url "https://github.com/Morpho-lang/morpho/archive/refs/tags/v0.6.1-alpha.tar.gz"
+  url "https://github.com/Morpho-lang/morpho/archive/refs/tags/v0.6.2-alpha.tar.gz"
   sha256 "b916aff186e2bbcc66d0a56c25eb50f861af14cc8c14119c19309f9fb10cb3c5"
   license "MIT"
 
@@ -13,7 +13,8 @@ class Morpho < Formula
       args = std_cmake_args
       args.delete "-DCMAKE_BUILD_TYPE=None"
       args << "-DCMAKE_BUILD_TYPE=Release"
-      args << "-DMORPHO_RESOURCESDIR=#{prefix}"
+      args << "-DMORPHO_HELP_BASEDIR=#{prefix}"
+      args << "-DMORPHO_MODULE_BASEDIR=#{prefix}"
       args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
       args << ".."
       system "cmake", *args
