2d3bdccb195fa0df6f4ad73ab5e3a3d61afa67a9
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2025-01-23 18:02:24 -0500

    Change url to latest version

--- Formula/morpho.rb
+++ Formula/morpho.rb
@@ -1,7 +1,7 @@
 class Morpho < Formula
   desc "The morpho language built as a shared library."
   homepage "https://github.com/Morpho-lang/morpho"
-  url "https://github.com/Morpho-lang/morpho/archive/refs/tags/v0.6.2-alpha.tar.gz"
+  url "https://github.com/Morpho-lang/morpho/archive/refs/tags/v0.6.2.tar.gz"
   sha256 "3fc1cd3c1eb396ba294f7d28b49191d13b7eb2b4dac584cab14091ad8538a86d"
   license "MIT"
 
