5c3b363be540e5aac473e2d517ddfa67e1b9233c
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2025-08-26 18:00:42 -0400

    Update morpho.rb

--- Formula/morpho.rb
+++ Formula/morpho.rb
@@ -2,7 +2,7 @@ class Morpho < Formula
   desc "The morpho language built as a shared library."
   homepage "https://github.com/Morpho-lang/morpho"
   url "https://github.com/Morpho-lang/morpho/archive/refs/tags/v0.6.3.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "b173c2434778eb925ec66144baad31c9626e6f29041ff8d06688c68ef02be181"
   license "MIT"
 
   depends_on "cmake" => :build
