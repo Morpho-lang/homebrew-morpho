dcd8b45c59fba1b47b7da31724990dac4845ea12
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2026-07-08 13:59:25 -0400

    Correct sha256 checksum

--- Formula/morpho-morphopm.rb
+++ Formula/morpho-morphopm.rb
@@ -2,7 +2,7 @@ class MorphoMorphopm < Formula
   desc "Morpho-morphom. Simple package manager for the morpho language"
   homepage "https://github.com/morpho-lang/morpho-morphopm"
   url "https://github.com/Morpho-lang/morpho-morphopm/archive/refs/tags/v0.2.0-alpha2.tar.gz"
-  sha256 "5484213858b39eda883bf06a07da7ae222a3715971f2d35c7ade2a7f1dafd1b2"
+  sha256 "50d64e7b4349fe0b7d1875c4f20bee6ce4cae236d4b328109307ed34f8aeb7f1"
   license "MIT"
 
   depends_on "cmake" 
