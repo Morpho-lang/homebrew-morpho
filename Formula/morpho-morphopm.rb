4231c144bec1d048ad86844020c44eab4250e429
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2026-07-08 13:55:58 -0400

    Update morpho-morphopm.rb

--- Formula/morpho-morphopm.rb
+++ Formula/morpho-morphopm.rb
@@ -1,7 +1,7 @@
 class MorphoMorphopm < Formula
   desc "Morpho-morphom. Simple package manager for the morpho language"
   homepage "https://github.com/morpho-lang/morpho-morphopm"
-  url "https://github.com/Morpho-lang/morpho-morphopm/archive/refs/tags/v0.2.0-alpha1.tar.gz"
+  url "https://github.com/Morpho-lang/morpho-morphopm/archive/refs/tags/v0.2.0-alpha2.tar.gz"
   sha256 "5484213858b39eda883bf06a07da7ae222a3715971f2d35c7ade2a7f1dafd1b2"
   license "MIT"
 
@@ -13,6 +13,6 @@ class MorphoMorphopm < Formula
   end
 
   test do
-    assert_equal "0.1.0", shell_output("#{bin}/morphopm version").strip.gsub(/\e\[(\d+)(;\d+)*m/, '') # Remove terminal codes
+    assert_equal "0.2.0", shell_output("#{bin}/morphopm version").strip.gsub(/\e\[(\d+)(;\d+)*m/, '') # Remove terminal codes
   end
 end
