032e2388fe4603b8cae2892031727261e53155ef
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2026-07-23 09:10:07 -0400

    Update morpho-morphopm.rb

--- Formula/morpho-morphopm.rb
+++ Formula/morpho-morphopm.rb
@@ -1,8 +1,8 @@
 class MorphoMorphopm < Formula
   desc "Morpho-morphom. Simple package manager for the morpho language"
   homepage "https://github.com/morpho-lang/morpho-morphopm"
-  url "https://github.com/Morpho-lang/morpho-morphopm/archive/refs/tags/v0.2.0-alpha5.tar.gz"
-  sha256 "e795ccc710adca86f51ef624f18684d7ed9ad86ff00052e371795da3c315a026"
+  url "https://github.com/Morpho-lang/morpho-morphopm/archive/refs/tags/v0.2.0-alpha6.tar.gz"
+  sha256 "927f9c63ba32912c18e74ce6e158821ef946cacb9aaa6fc355a3a8d6f31abcfd"
   license "MIT"
 
   depends_on "cmake" 
